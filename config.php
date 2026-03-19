33d9929a90b35814bcd409e54bd6c9e4337f72b3
hploompuu-spec <hploompuu@hkhk.edu.ee>
2026-03-19 11:10:20 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,15 +1,18 @@
-<?php
-    // Sinu andmed
-    $db_server = 'localhost';
-    $db_andmebaas = 'car_rent';
-    $db_kasutaja = 'hannes';
-    $db_salasona = 'Passw0rd';
+<?php 
+// *** Protseduuriline *** //
+// Sinu andmed
+$db_server = 'localhost';
+$db_andmebaas = 'car_rent';
+$db_kasutaja = 'hannes';
+$db_salasona = 'Passw0rd';
 
-    // Ühendus andmebaasiga
-    $yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
 
-    // Ühenduse kontroll
-    if (!$yhendus) {
-        die('Ei saa ühendust andmebaasiga');
-    }
+// Ühendus andmebaasiga
+$yhendus = mysqli_connect($db_server, $db_kasutaja, $db_salasona, $db_andmebaas);
+
+
+// Ühenduse kontroll
+if (!$yhendus) {
+    die('Ei saa ühendust andmebaasiga');
+}
 ?>
