236fd397bc872c1cfa5ec8e5cf1c093ec72f8c5c
hploompuu-spec <hploompuu@hkhk.edu.ee>
2026-03-19 12:13:02 +0200

    Add files via upload

--- admin/index.php
+++ admin/index.php
@@ -15,58 +15,52 @@
     // var_dump($_GET["otsi"]);
 
     $valjund = mysqli_query($yhendus, $paring); //saadan päringu andmebaasi
-    while($rida = mysqli_fetch_assoc($valjund)){       //sikutan vastuse alla
+    
         // var_dump($rida);                            //kuvan testvastuse
 ?>
+<h2>Adminni ala</h2>
+<a href="lisa.php" class="btn btn-success">+ Lisa auto</a>
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">Mark</th>
+      <th scope="col">Mudel</th>
+      <th scope="col">Mootor</th>
+      <th scope="col">Kütus</th>
+      <th scope="col">Aasta</th>
+      <th scope="col">Käigukast</th>
+      <th scope="col">Istekohti</th>
+      <th scope="col">Iseloomustus</th>
+      <th scope="col">Staatus</th>
+      <th scope="col">Hind</th>
     </tr>
   </thead>
   <tbody>
+    <?php
+    while($rida = mysqli_fetch_assoc($valjund)){       //sikutan vastuse alla
+
+    ?>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row"><?php echo $rida["id"]; ?></th>
+      <td><?php echo $rida["mark"]; ?></td>
+      <td><?php echo $rida["model"]; ?></td>
+      <td><?php echo $rida["engine"]; ?></td>
+      <td><?php echo $rida["fuel"]; ?></td>
+      <td><?php echo $rida["year"]; ?></td>
+      <td><?php echo $rida["transmission"]; ?></td>
+      <td><?php echo $rida["seats"]; ?></td>
+      <td><?php echo $rida["description"]; ?></td>
+      <td><?php echo $rida["status"]; ?></td>
+      <td><?php echo $rida["price"]; ?></td>
+      <td><a href="kustuta.php?editid=<?= $rida["id"]; ?></a>" class="btn btn-warning">Muuda</a></td>
+      <td><a href="kustuta.php?delid=<?= $rida["id"]; ?></a>" class="btn btn-danger">Kustuta</a></td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>John</td>
-      <td>Doe</td>
-      <td>@social</td>
-    </tr>
+  
+  <?php } ?>
   </tbody>
 </table>
-    <div class="col">
-        <div class="card">
-        <img src="https://loremflickr.com/400/250/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top" alt="<?php echo $rida["mark"]; ?>">
-        <div class="card-body">
-            <h5 class="card-title"><?php echo $rida["mark"]; ?> <?php echo $rida["model"]; ?></h5>
-            <p class="card-text">
-                Mootor: <?php echo $rida["engine"]; ?> <br>
-                Kütus: <?php echo $rida["fuel"]; ?><br>
-                Hind: <?php echo $rida["price"]; ?>€/päev<br>
-            </p>
-            <a href="single_car.php?id=<?php echo $rida["id"]; ?>" class="btn btn-dark w-100">Rendi</a>
-        </div>
-        </div>
-    </div>
-    <?php } ?>
-        <!-- /üks auto -->
-    </div>
-</div>
-<!-- /sisu -->
+    
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
