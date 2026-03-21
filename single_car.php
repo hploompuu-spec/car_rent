ab2e6d1b3eb989b55a69df2ade8fda3d4bbbc079
hploompuu-spec <hploompuu@hkhk.edu.ee>
2026-03-21 17:16:43 +0200

    Puhas install

--- single_car.php
+++ single_car.php
@@ -1,7 +1,6 @@
 <?php include('config.php'); ?>
 <?php include('header.php'); ?>
 
-
 <!-- sisu -->
 <div class="container">
 
@@ -13,13 +12,18 @@
     $paring = "SELECT * FROM cars WHERE id=".$id."";
     $valjund = mysqli_query($yhendus, $paring);
     $rida = mysqli_fetch_assoc($valjund);
-    // print_r($rida);
+     print_r($rida);
 ?>
         <div class="col">
             <h1><?php echo $rida["mark"]; ?> <?php echo $rida["model"]; ?></h1>
             <p>Mootor:  <?php echo $rida["engine"]; ?></p>
             <p>Kütus:  <?php echo $rida["fuel"]; ?></p>
-            <p>Hind:  <?php echo $rida["price"]; ?></p>
+            <p>Aasta:  <?= $rida["year"]; ?></p>
+            <p>Staatus:  <?php echo $rida["status"]; ?></p>
+            <p>Käigukast:  <?php echo $rida["transmission"]; ?></p>
+            <p>Istmed:  <?php echo $rida["seats"]; ?></p>
+            <p class="fs-5">Hind:  <?php echo $rida["price"]; ?></p>
+            <a href="#" class="btn btn-dark w-100">Rendi auto</a>
         </div>
         <div class="col">
             <img src="https://loremflickr.com/800/500/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top img-fluid" alt="<?php echo $rida["mark"]; ?>">
