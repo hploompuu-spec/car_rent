2cd103cd7324310c9ea08f1d3b4631c69b7bdba6
hploompuu-spec <hploompuu@hkhk.edu.ee>
2026-03-25 20:20:59 +0200

    Mockaroo genereeritudfailid

---
+++ db/users.sql
@@ -0,0 +1,30 @@
+create table users (
+	id INT,
+	role VARCHAR(14),
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	phone VARCHAR(50),
+	password_hash VARCHAR(50),
+	created_at DATE
+);
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (1, 'kasutaja', 'Farlay', 'Epton', 'fepton0@newyorker.com', '167-860-9943', '$2a$04$SIVZd2zBzR132G3ZNxIJ2.P7.szhmflHgo4DnVt1dfuV6HoHj/gA2', '2019-05-03 01:19:54');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (2, 'lugeja', 'Ashil', 'Hubbuck', 'ahubbuck1@etsy.com', '108-842-5994', '$2a$04$DBXdRJYOdXc3D3VbbsJrRe/ywYF8.9eXeEgpLvttS4Cd0nECeXa5i', '2026-06-12 09:08:43');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (3, 'lugeja', 'Sally', 'Martschke', 'smartschke2@wired.com', '203-130-6504', '$2a$04$Mjt3MRhnlJEVdQonga9A6.2lAoAqsH4wIqJxgEO9pooF7FSYKxkpG', '2025-12-07 09:59:01');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (4, 'kasutaja', 'Claiborne', 'Falconer-Taylor', 'cfalconertaylor3@addthis.com', '415-600-8856', '$2a$04$SaO7MpEXRfucO0VwZRxZg.lM82RvcfCNoNzKdBJmjMKoyQf8FasqW', '2021-12-26 05:26:48');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (5, 'kasutaja', 'Cati', 'Le Page', 'clepage4@cnn.com', '518-268-7547', '$2a$04$BMs67oX.WUkFUWO81hah5eqzVT1PiA24.ARPNKFN.jU0H4NOnSJE.', '2018-12-23 08:35:02');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (6, 'administraator', 'Robb', 'Faichnie', 'rfaichnie5@indiatimes.com', '507-849-9681', '$2a$04$lFGNAB3AS8mrMYpdYOdukOmrJKUCX4iypIQ0LBQ2XHCFdunTf0d8m', '2021-09-30 16:20:27');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (7, 'kasutaja', 'Ivett', 'Erley', 'ierley6@oracle.com', '693-407-7547', '$2a$04$AUVkjVG.nJ.XlHD2NTS/hO1rX6x/v/EmxPYGO66bkWwLyVfe79OY6', '2025-08-10 13:45:57');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (8, 'kasutaja', 'Friederike', 'Lux', 'flux7@diigo.com', '424-192-7754', '$2a$04$HcSX.55NDNq2Q6hFfc50Xej1HOZTr2CCr3KMzStVhzpKnsQDx0Pma', '2022-09-04 15:39:52');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (9, 'lugeja', 'Eveleen', 'Got', 'egot8@ovh.net', '203-839-9119', '$2a$04$VJYuGjl144lfPy8v2qK5.ugndghwLipt5y.j3aqNPX1MdGYZOXmmG', '2026-02-14 13:01:11');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (10, 'kasutaja', 'Randy', 'Whitehall', 'rwhitehall9@vkontakte.ru', '426-973-4770', '$2a$04$z92UDteZrbPkjvB/ZRQDIuVICE0/cjJ3gvoj/DNh7d92S/FjXLk5K', '2025-12-21 09:20:58');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (11, 'administraator', 'Hedvige', 'Pobjay', 'hpobjaya@biblegateway.com', '431-210-6634', '$2a$04$C1P1r8OqXnwW3Kxnj.yX/O93hKlF9Ux9bTo37bAppF4DBXMcwoKqS', '2023-07-31 01:31:23');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (12, 'kasutaja', 'Isahella', 'Still', 'istillb@toplist.cz', '809-766-4978', '$2a$04$6A5BP/CzDEb9b8Pzbqd3c.LYuFDUrrFSqBuMut1v7.npeUNmfEdOS', '2022-10-02 07:40:30');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (13, 'administraator', 'Jilleen', 'Jahndel', 'jjahndelc@goodreads.com', '175-427-1392', '$2a$04$pcviu4L0UpdlZYyyWUUI6ehyMThR7ep9d/K4M17bYFGaU7.pyIkUK', '2020-03-12 23:37:10');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (14, 'kasutaja', 'Gregg', 'Kiff', 'gkiffd@behance.net', '920-640-8176', '$2a$04$vWXGJSxz8LE2ics5TobqWejf8TYyup3V.kYMrqnHxQhAPJXFl5JzS', '2021-01-07 15:24:57');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (15, 'administraator', 'Domenico', 'Tompsett', 'dtompsette@ebay.co.uk', '373-932-6669', '$2a$04$ZD74c2OEan8IXcH4NLeOk.IVuG3ocAUOIKkEs6fVhWzmnRDsOR90C', '2021-12-25 04:10:28');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (16, 'kasutaja', 'Franchot', 'Zammett', 'fzammettf@illinois.edu', '976-475-8592', '$2a$04$xsp8S2NQmw3TMTdFKThDXei87JztqXKqvWi9wPoPTho8pbdNAtMpW', '2020-08-28 17:32:04');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (17, 'lugeja', 'Christel', 'Duxbarry', 'cduxbarryg@netscape.com', '328-693-7505', '$2a$04$cr151SQ.Y0B.3L2sZy7xxuBlAxm.EUpORRoalrx/csbCR4ywl2V5i', '2017-10-22 22:10:45');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (18, 'lugeja', 'Vilma', 'Edinburough', 'vedinburoughh@lulu.com', '330-502-9701', '$2a$04$kbj93fQ6UHQ47ywZmyafPuOrreOuNnxjxg9iw8PVHSwkkf0Khhb/.', '2016-08-16 21:00:04');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (19, 'lugeja', 'Fanni', 'Lavender', 'flavenderi@biglobe.ne.jp', '489-202-9227', '$2a$04$5KGYwno.0qDc9lRVHc0xR.jhykDsqvLdbPR4aTibcZmRBT6zCAp3C', '2023-05-08 20:49:27');
+insert into users (id, role, first_name, last_name, email, phone, password_hash, created_at) values (20, 'lugeja', 'Lev', 'Martyntsev', 'lmartyntsevj@live.com', '980-657-2790', '$2a$04$rQxSeuF/kdAhvPRbNiJ4iO0ABvx7H8NCaGLS0/huWvLd6JgMM11W.', '2026-05-31 21:27:54');
