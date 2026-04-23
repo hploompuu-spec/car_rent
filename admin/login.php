c009a6167f37370010a2cae7e803180f01b63748
hploompuu-spec <hploompuu@hkhk.edu.ee>
2026-04-23 14:13:06 +0300

    poolel

--- admin/login.php
+++ admin/login.php
@@ -7,21 +7,41 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>    
+<?php
+    $msg = "";
+    
+    
+    if (!empty($_POST)) {
+        $uname = $_POST['user'];
+        $password = $_POST['password'];
+        $hash = '$2y$10$3WGG.HHGMnrTqI1/SX2UCOy/ZLGs.O4MIlVkh3FKSVwrVFngOJMjS';
+        
+        if ($uname=="admin" && password_verify($password, $hash)) {
+            echo "tere admin";
+        }else{
+            $msg = "kasutaja vale";
+        }
+    }
+   
+    
+    
+?>
     <div class="container">
         <div class="row">
             <div class="col-sm-4"></div>
             <div class="col-sm-4">
-                <form>
+                <form method="post" action="login.php">
                      <div class="mb-3">
                          <label for="exampleInputEmail1" class="form-label">Username</label>
                           <input name="user" type="text" class="form-control" id="exampleInputEmail1">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input name = "password" type="password" class="form-control" id="exampleInputPassword1">
                     </div>
                     <button type="submit" class="btn btn-primary">Logi sisse</button>
                 </form>
+                <?php echo  $msg; ?>
             </div>
             <div class="col-sm-4"></div>
         </div>
