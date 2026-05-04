facf4aedb88ecc49a68bbef518d5adac1718b02b
hploompuu-spec <hploompuu@hkhk.edu.ee>
2026-05-04 18:05:40 +0300

    muudetud

--- header.php
+++ header.php
@@ -18,19 +18,14 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" href="index.php">Home</a>
+          <a class="nav-link active" href="index.php">Pealehele</a>
         </li>
         <?php if (!empty($_SESSION['tuvastamine'])): ?>
         <li class="nav-item">
           <a class="nav-link" href="/car_rent/my_rentals.php">Minu broneeringud</a>
         </li>
         <?php endif; ?>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
+
       </ul>
       <form class="d-flex" role="search" method="get" action="index.php">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="otsi">
